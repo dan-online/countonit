2d535c1cb48f72f9805500a17927c56ad617f678
DanCodes <dan@dancodes.online>
2022-06-20 13:10:24 +0200

    fix(head): move to app sub property

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,54 +4,55 @@ import ViteComponents from "unplugin-vue-components/vite";
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   ssr: false,
-  head: {
-    title: "Count on it - Home",
-    meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { name: "msapplication-TileColor", content: "#da532c" },
-      { name: "theme-color", content: "#ffffff" },
-      { name: "title", content: "Count on it - Home" },
-      {
-        name: "description",
-        content:
-          "Count on it is a simple and easy-to-use countdown website that lets you create powerful countdowns to your dates in 3 clicks.",
-      },
-      { property: "og:type", content: "website" },
-      { property: "og:title", content: "Count on it - Home" },
-      {
-        property: "og:description",
-        content:
-          "Count on it is a simple and easy-to-use countdown website that lets you create powerful countdowns to your dates in 3 clicks.",
-      },
-      { property: "twitter:title", content: "Count on it - Home" },
-      {
-        property: "twitter:description",
-        content:
-          "Count on it is a simple and easy-to-use countdown website that lets you create powerful countdowns to your dates in 3 clicks.",
-      },
-    ],
-    link: [
-      {
-        rel: "apple-touch-icon",
-        sizes: "180x180",
-        href: "/apple-touch-icon.png",
-      },
-      {
-        rel: "icon",
-        type: "image/png",
-        sizes: "32x32",
-        href: "/favicon-32x32.png",
-      },
-      {
-        rel: "icon",
-        type: "image/png",
-        sizes: "16x16",
-        href: "/favicon-16x16.png",
-      },
-      { rel: "manifest", href: "/site.webmanifest" },
-      { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#5bbad5" },
-    ],
+  app: {
+    head: {
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { name: "msapplication-TileColor", content: "#da532c" },
+        { name: "theme-color", content: "#ffffff" },
+        { name: "title", content: "Count on it - Home" },
+        {
+          name: "description",
+          content:
+            "Count on it is a simple and easy-to-use countdown website that lets you create powerful countdowns to your dates in 3 clicks.",
+        },
+        { property: "og:type", content: "website" },
+        { property: "og:title", content: "Count on it - Home" },
+        {
+          property: "og:description",
+          content:
+            "Count on it is a simple and easy-to-use countdown website that lets you create powerful countdowns to your dates in 3 clicks.",
+        },
+        { property: "twitter:title", content: "Count on it - Home" },
+        {
+          property: "twitter:description",
+          content:
+            "Count on it is a simple and easy-to-use countdown website that lets you create powerful countdowns to your dates in 3 clicks.",
+        },
+      ],
+      link: [
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon-32x32.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon-16x16.png",
+        },
+        { rel: "manifest", href: "/site.webmanifest" },
+        { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#5bbad5" },
+      ],
+    },
   },
   buildModules: [
     "nuxt-windicss",
